8b4e0d1c10b3a365c42514fd4dc1687edf3f5fa7
L-z-r-s <slazarus20002gmail.com>
2024-07-20 12:23:37 +0530

    20-7

---
+++ Includes/register.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+if ($_SERVER[REQUEST_METHOD])=="POST"{
+    $name= $_POST["name"];
+    $pr_topic=$_POST["pr_topic"];
+    $email=$_POST["email"];
+    $c_name=$_POST["c_name"];
+    $dept
+}
